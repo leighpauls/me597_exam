464e372ee7f072d1681afa7c78e8dc87444eed66
Leigh Pauls <leigh.pauls@gmail.com>
2013-12-09 18:19:38 -0500

    Q2 Part B Don't print the path

--- Q2/partB.m
+++ Q2/partB.m
@@ -7,7 +7,7 @@ connections = makeConnectionMatrix(IDX25, IDXout25, IDX50, IDXout50);
 color_strings = ['y' 'c' 'r' 'g' 'k' 'y' 'c' 'r' 'g' 'k'];
 for depot_idx = [1:2]
   for copter_idx = [(depot_idx-1)*5+1 : depot_idx*5]
-    path = findPath(connections, depot_nodes(depot_idx), drop_nodes(copter_idx))
+    path = findPath(connections, depot_nodes(depot_idx), drop_nodes(copter_idx));
     plotPath(path, length(connections)/2, node_locations, color_strings(copter_idx));
     % remove that path from the connection matrix
     connections = removePathFromConnections(path, connections);
